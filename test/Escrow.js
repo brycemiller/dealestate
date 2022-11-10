28c5f140e546272f587948fd8bd76230c199aa9b
Bryce Miller <bryce.a.miller@googlemail.com>
2022-11-10 21:43:56 +0100

    [DEAL-6] Added function for listing a property.  Added state and purchase details as mapping.  Added tests.

--- test/Escrow.js
+++ test/Escrow.js
@@ -30,6 +30,15 @@ describe('Escrow', () => {
             inspector.address,
             lender.address
         );
+
+        // Approve property
+        transaction = await realEstate.connect(seller).approve(escrow.address, 1);
+        await transaction.wait();
+
+        // List property
+        transaction = await escrow.connect(seller).
+            listProperty(1, buyer.address, tokens(10), tokens(5));
+        await transaction.wait();
     });
 
     describe('Deployment', () => {
@@ -51,6 +60,39 @@ describe('Escrow', () => {
         it('Returns Lender', async () => {
             const result = await escrow.lender();
             expect(result).to.be.equal(lender.address);
+        });
+    });
+
+    describe('Listing', () => {
+        it('Updates ownership', async () => {
+            const result = await escrow.isListed(1);
+            expect(result).to.be.equal(true);
+        });
+
+        it('Updates as listed', async () => {
+            expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address);
+        });
+
+        it('Returns buyer', async () => {
+            const result = await escrow.buyer(1);
+            expect(result).to.be.equal(buyer.address);
+        });
+
+        it('Returns purchase price', async () => {
+            const result = await escrow.purchasePrice(1);
+            expect(result).to.be.equal(tokens(10));
+        });
+
+        it('Returns escrow amount', async () => {
+            const result = await escrow.escrowAmount(1);
+            expect(result).to.be.equal(tokens(5));
+        });
+
+        it('Only allows seller to list property', async () => {
+            expect(
+                escrow.connect(inspector).
+                    listProperty(1, buyer.address, tokens(10), tokens(5))
+            ).to.be.revertedWith("Only the seller can call this method");
         });
     });
 })
