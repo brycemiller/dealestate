45d6f8a04b69da769daee1700fabac8fbaa91c19
Bryce Miller <bryce.a.miller@googlemail.com>
2022-11-17 20:20:39 +0100

    [DEAL-12] Add approval mapping for sale approval by buyer, seller, and inspector

--- test/Escrow.js
+++ test/Escrow.js
@@ -142,4 +142,21 @@ describe('Escrow', () => {
             ).to.be.revertedWith("Only the inspector can call this method");
         });
     });
+
+    describe('Approval', () => {
+        it('Updates approval status', async () => {
+            let transaction = await escrow.connect(buyer).approveSale(1);
+            await transaction.wait();
+
+            transaction = await escrow.connect(seller).approveSale(1);
+            await transaction.wait();
+
+            transaction = await escrow.connect(lender).approveSale(1);
+            await transaction.wait();
+
+            expect(await escrow.approval(1, buyer.address)).to.be.equal(true);
+            expect(await escrow.approval(1, seller.address)).to.be.equal(true);
+            expect(await escrow.approval(1, lender.address)).to.be.equal(true);
+        });
+    });
 })
