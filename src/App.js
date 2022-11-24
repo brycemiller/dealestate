bd5b1a659e68077b9ac9747e58171a26951a4d36
Bryce Miller <bryce.a.miller@googlemail.com>
2022-11-24 19:51:41 +0100

    [DEAL-21] Added navigation bar and account connection/handling

--- src/App.js
+++ src/App.js
@@ -19,8 +19,12 @@ function App() {
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    setAccount(accounts[0]);
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
   };
 
   useEffect(() => {
@@ -29,7 +33,7 @@ function App() {
 
   return (
     <div>
-
+      <Navigation account={account} setAccount={setAccount} />
       <div className='cards__section'>
 
         <h3>Welcome to Millow</h3>
